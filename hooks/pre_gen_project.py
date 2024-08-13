b6fb51494ee3727409dd1bd05dffa175aa9e3d80
Benjmain Fair <bfair.kun@gmail.com>
2024-08-13 14:25:26 -0500

    check for valid module names

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 """
-Pre cookiecutter hook script to check that some template variables are valid. Specifically, no weird characters in the repo_name.
+Pre cookiecutter hook script to check that some template variables are valid. Specifically, no weird characters in the repo_name, only and only word characters [a-zA-Z0-9_] in module names.
 """
 
 import re
 import sys
+import collections
 
 
 REPO_REGEX = r'^[_a-zA-Z\-0-9]+$'
@@ -16,3 +17,11 @@ if not re.match(REPO_REGEX, repo_name):
     print('ERROR: %s is not a valid name for repo_name' % repo_name)
     # exits with status 1 to indicate failure
     sys.exit(1)
+
+submodules = eval("{{ cookiecutter.submodules }}", {"OrderedDict": collections.OrderedDict})
+
+for submodule in submodules.keys():
+    if not re.match(r'^\w+$', submodule):
+        print('ERROR: %s is not a valid name for repo_name' % submodule)
+        sys.exit(1)
+
